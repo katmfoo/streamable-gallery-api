31186cef0cf25a3e955a7d009344ea5c9572d6e0
Pat Richeal <patrickricheal@gmail.com>
2017-11-12 23:08:48 -0500

    Basic request forward that sends the correct cookies, copies over query params, and returns the response

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+
+const url = 'https://ajax.streamable.com/videos'; // base url
+
+const header = {
+  cookie: 'user_name=patrickricheal@gmail.com; user_code=eyJhbGciOiJIUzI1NiIsImV4cCI6MTU0MjA3MjM4MCwiaWF0IjoxNTEwNTM2MzgwfQ.eyJ1c2VyX25hbWUiOiJwYXRyaWNrcmljaGVhbEBnbWFpbC5jb20ifQ.08fu01EzozfLl1E9ENk80Bi-XS_iabQ72tcRxZ_i0vk;'
+};
+
+/*
+ * Possible query parameters
+ *   1. sort, possible values are date_added, plays, title
+ *   2. sortd, possible values are ASC, DESC
+ *   3. count
+ *   4. page
+ */
+app.get('/', (req, res) => {
+  axios.get(url, {headers: header, params: req.query})
+  .then(response => {
+    res.send(response.data);
+  })
+  .catch(error => {
+    res.send(error);
+  });
+
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
